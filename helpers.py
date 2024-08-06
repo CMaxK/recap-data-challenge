935244c98048ba2ddac73ef09c21d750e6a66645
Christopher Karg <christopher_karg@yahoo.co.uk>
2024-08-06 15:25:15 +0100

    add black to refactor code on push

--- helpers.py
+++ helpers.py
@@ -2,7 +2,10 @@ import requests
 import json
 import pandas as pd
 
-BASE_URL = "https://nookdtmzylu7w75p7atatnzom40zmdpz.lambda-url.eu-central-1.on.aws/invoices"
+BASE_URL = (
+    "https://nookdtmzylu7w75p7atatnzom40zmdpz.lambda-url.eu-central-1.on.aws/invoices"
+)
+
 
 def fetch_all_invoices():
     """
@@ -11,7 +14,7 @@ def fetch_all_invoices():
     """
     invoices = []
     page = 1
-    total_pages = 1 #allows matching of current page with amx page
+    total_pages = 1  # allows matching of current page with amx page
 
     while page <= total_pages:
         try:
@@ -20,12 +23,14 @@ def fetch_all_invoices():
             response.raise_for_status()
 
             response_data = response.json()
-            if response_data.get('status_code') != 200:
-                print(f"Error: Status code {response_data.get('status_code')} on page {page}")
+            if response_data.get("status_code") != 200:
+                print(
+                    f"Error: Status code {response_data.get('status_code')} on page {page}"
+                )
                 break
 
-            data = response_data.get('body', {}).get('data', [])
-            total_pages = response_data.get('body', {}).get('total_pages', 1)
+            data = response_data.get("body", {}).get("data", [])
+            total_pages = response_data.get("body", {}).get("total_pages", 1)
 
             if not data and page > total_pages:
                 break
@@ -46,6 +51,7 @@ def fetch_all_invoices():
 
     return invoices
 
+
 def save_invoices_to_file(invoices, filename="data/invoices.json"):
     """
     Writes invoices to file called invoices.json
@@ -53,25 +59,31 @@ def save_invoices_to_file(invoices, filename="data/invoices.json"):
     with open(filename, "w") as f:
         json.dump(invoices, f, indent=4)
 
+
 def preprocess_invoices(invoices):
     """
     Handles grouping and aggregating of monthly revenus for each contract_id
     """
     df = pd.DataFrame(invoices)
-    df['invoice_date'] = pd.to_datetime(df['invoice_date'])
+    df["invoice_date"] = pd.to_datetime(df["invoice_date"])
 
     # create a new 'month' column
-    df['month'] = df['invoice_date'].dt.to_period('M')
+    df["month"] = df["invoice_date"].dt.to_period("M")
 
     # group by contract_id and month, sum the original_billing_amount
-    df = df.groupby(['contract_id', 'month']).agg({'original_billing_amount': 'sum'}).reset_index()
+    df = (
+        df.groupby(["contract_id", "month"])
+        .agg({"original_billing_amount": "sum"})
+        .reset_index()
+    )
 
-    df.rename(columns={'original_billing_amount': 'net_revenue'}, inplace=True)
+    df.rename(columns={"original_billing_amount": "net_revenue"}, inplace=True)
 
     # not strictly necessary as previous grouping handles sorting already
-    df = df.sort_values(by=['contract_id', 'month'])
+    df = df.sort_values(by=["contract_id", "month"])
 
     return df
+
 
 def compute_net_revenue_and_churn(df):
     """
@@ -79,22 +91,29 @@ def compute_net_revenue_and_churn(df):
     contract.
     Outputs cleaned DF
     """
-    df['previous_net_revenue'] = df.groupby('contract_id')['net_revenue'].shift().fillna(0)
-
-    # Calculate churned amount
-    df['churned_amount'] = df.apply(
-        lambda row: row['previous_net_revenue'] - row['net_revenue'] if row['previous_net_revenue'] > row['net_revenue'] else 0,
-        axis=1
+    df["previous_net_revenue"] = (
+        df.groupby("contract_id")["net_revenue"].shift().fillna(0)
     )
 
-    df = df.drop(columns=['previous_net_revenue'])
-    df['net_revenue'] = df['net_revenue'].round(2)
-    df['churned_amount'] = df['churned_amount'].round(2)
+    # Calculate churned amount
+    df["churned_amount"] = df.apply(
+        lambda row: (
+            row["previous_net_revenue"] - row["net_revenue"]
+            if row["previous_net_revenue"] > row["net_revenue"]
+            else 0
+        ),
+        axis=1,
+    )
+
+    df = df.drop(columns=["previous_net_revenue"])
+    df["net_revenue"] = df["net_revenue"].round(2)
+    df["churned_amount"] = df["churned_amount"].round(2)
 
     # Final formatting
-    df['month'] = df['month'].astype(str)
+    df["month"] = df["month"].astype(str)
 
     return df
+
 
 def save_net_revenue_churn_to_file(df, filename="data/net_revenue_churn.csv"):
     """
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from helpers import fetch_all_invoices, save_invoices_to_file, preprocess_invoices, compute_net_revenue_and_churn, save_net_revenue_churn_to_file
+from helpers import (
+    fetch_all_invoices,
+    save_invoices_to_file,
+    preprocess_invoices,
+    compute_net_revenue_and_churn,
+    save_net_revenue_churn_to_file,
+)
+
+
 def main():
     invoices = fetch_all_invoices()
     save_invoices_to_file(invoices)
